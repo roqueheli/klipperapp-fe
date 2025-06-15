99903e230d9d3ba9ce62af6e35c9e38b5c27fab0
Roqueheli <roqueheliarenas@gmail.com>
2025-06-15 10:31:42 -0400

    deleling request from apis

--- src/schemes/register.scheme.ts
+++ src/schemes/register.scheme.ts
@@ -2,12 +2,11 @@ import * as Yup from "yup";
 
 const RegisterScheme = Yup.object().shape({
     email: Yup.string().email('Invalid email').required('Email is required'),
-    password: Yup.string().min(8, 'Password must be at least 8 characters').optional(),
+    password: Yup.string().min(8, 'Password must be at least 8 characters').notRequired(),
+    confirm_password: Yup.string().oneOf([Yup.ref('password')], 'Passwords must match').notRequired(),
     name: Yup.string().required('Name is required'),
     phone_number: Yup.string().required('Phone Number is required'),
     birth_date: Yup.date().required('Birthdate is required'),
-});
-
-export type RegisterData = Yup.InferType<typeof RegisterScheme>;
+}).strict();
 
 export default RegisterScheme;
