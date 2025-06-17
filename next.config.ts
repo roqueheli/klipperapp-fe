487f9517ba0181f012438b1019d86dc58859333e
Roqueheli <roqueheliarenas@gmail.com>
2025-06-16 20:33:43 -0400

    Revertir al commit 57d3e38

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,11 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, 'src');
+    return config;
+  },
   images: {
     domains: ["www.lemoustache.cl", 'instagram.fscl9-1.fna.fbcdn.net', "instagram.fscl9-2.fna.fbcdn.net"],
   },
