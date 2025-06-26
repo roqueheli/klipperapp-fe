2f878b629ec940af0c77a54638e042307a9c1dcb
Roqueheli <roqueheliarenas@gmail.com>
2025-06-26 15:07:43 -0400

    adding pagination to payments management page

--- src/lib/common/http.external.service.ts
+++ src/lib/common/http.external.service.ts
@@ -1,9 +1,9 @@
 import { HttpBaseAPI } from "./http.service";
 
-export const API_URL = process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_API_BASE_URL : "http://localhost:3000/api";
+export const API_URL = process.env.NODE_ENV === 'production' ? "https://klipperapp-be-6fbfe24ddcb3.herokuapp.com/api/v1" : (process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:3000/api");
 class HttpExternalAPI extends HttpBaseAPI {
     constructor() {
-        super(API_URL || "http://localhost:3000/api")
+        super(API_URL)
     }
 }
 
