99c6f4f116ca2579917906b918505afb47e9bd5f
Roqueheli <roqueheliarenas@gmail.com>
2025-07-10 14:53:06 -0400

    change addservice and end_attendance flux, implement cash reconciliation, implementing reopen attendance

---
+++ src/app/api/profiles/search/route.ts
@@ -0,0 +1,20 @@
+import profilesAPI from "@/lib/profiles/profiles.service";
+import { getToken } from "@/lib/utils/auth.utils";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+    const token = await getToken();
+    const searchParams = request.nextUrl.searchParams;    
+
+    if (!token) return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+
+    try {
+        const data = await profilesAPI.getProfileByQuery(searchParams, token);
+
+        if (!data) throw new Error('Customer not found');
+        
+        return NextResponse.json(data);
+    } catch (error) {
+        return new Response(JSON.stringify({ error: "Customer not found " + error, status: 404 }));
+    }
+}
--- src/components/attendances/wizard/PhoneStep.tsx
+++ src/components/attendances/wizard/PhoneStep.tsx
@@ -1,4 +1,10 @@
-import React from "react";
+"use client";
+
+import { useTheme } from "@/components/ThemeProvider";
+import httpInternalApi from "@/lib/common/http.internal.service";
+import { Profile } from "@/types/profile";
+import { Mail, Phone, User } from "lucide-react";
+import { useEffect, useState } from "react";
 
 interface PhoneStepProps {
   phone: string;
@@ -17,23 +23,151 @@ const PhoneStep: React.FC<PhoneStepProps> = ({
   onSubmit,
   onClose,
 }) => {
+  const { theme } = useTheme();
+  const [results, setResults] = useState<Profile[]>([]);
+  const [wasSelectedFromDropdown, setWasSelectedFromDropdown] = useState(false);
+  const [selectedProfile, setSelectedProfile] = useState<Profile | null>(null);
+
+  useEffect(() => {
+    if (wasSelectedFromDropdown) {
+      setWasSelectedFromDropdown(false); // resetea la bandera para próximos cambios manuales
+      return;
+    }
+
+    const fetchProfiles = async () => {
+      if (phone.length < 3) {
+        setResults([]);
+        return;
+      }
+
+      const params = new URLSearchParams();
+      params.set("query", phone);
+
+      try {
+        const res = (await httpInternalApi.httpGetPublic(
+          "/profiles/search",
+          params
+        )) as Profile[];
+
+        setResults(res || []);
+      } catch (e) {
+        console.error("Error al buscar:", e);
+        setResults([]);
+      }
+    };
+
+    const timeout = setTimeout(() => {
+      fetchProfiles();
+    }, 400);
+
+    return () => clearTimeout(timeout);
+  }, [phone]);
+
   return (
     <div className="w-full flex justify-center items-center flex-col min-h-screen text-center">
-      <h2 className="text-3xl font-extrabold mb-6 text-blue-600 dark:text-blue-400 drop-shadow-sm">
+      <h2
+        className={`text-3xl font-extrabold mb-6 ${
+          theme === "dark" ? "text-blue-400" : "text-blue-600"
+        } drop-shadow-sm`}
+      >
         Ingresa tu número de teléfono
       </h2>
-      <input
-        type="tel"
-        className="border placeholder:text-gray-400 dark:border-gray-700 px-4 py-3 rounded-md w-full max-w-sm mx-auto text-lg focus:outline-none focus:ring-2 focus:ring-blue-400 dark:focus:ring-blue-500 transition"
-        placeholder="Ej: 9 1234 5678"
-        value={phone}
-        onChange={(e) => onPhoneChange(e.target.value)}
-      />
+
+      <div className="relative w-full max-w-lg mx-auto">
+        <input
+          type="text"
+          className={`border placeholder:text-gray-400 ${
+            theme === "dark"
+              ? "border-gray-700 focus:ring-blue-500"
+              : "focus:ring-blue-400"
+          } px-4 py-3 rounded-md w-full text-lg focus:outline-none focus:ring-2 transition`}
+          placeholder="Teléfono, nombre o correo"
+          value={phone}
+          onChange={(e) => {
+            const value = e.target.value;
+            onPhoneChange(value);
+            if (value === "") setSelectedProfile(null);
+          }}
+        />
+
+        {results.length > 0 && (
+          <ul
+            className={`absolute z-10 ${
+              theme === "dark" ? "bg-gray-800 border-gray-700" : "bg-white"
+            } border w-full rounded-md shadow-lg mt-1 max-h-60 overflow-y-auto`}
+          >
+            {results.map((r) => (
+              <li
+                key={r.id}
+                onClick={() => {
+                  onPhoneChange(r.phone_number);
+                  setSelectedProfile(r);
+                  setResults([]);
+                  setWasSelectedFromDropdown(true);
+                }}
+                className={`flex gap-2 items-center p-3 ${
+                  theme === "dark"
+                    ? "hover:bg-blue-900 border-gray-700"
+                    : "hover:bg-blue-100 border-gray-100"
+                } cursor-pointer text-left transition-colors border-b`}
+              >
+                <div className="flex items-center justify-center gap-2 mb-1">
+                  <User
+                    className={`w-4 h-4 ${
+                      theme === "dark" ? "text-gray-400" : "text-gray-500"
+                    }`}
+                  />
+                  <span className="font-small capitalize truncate">
+                    {r.name}
+                  </span>
+                </div>
+                <div
+                  className={`flex items-center justify-center gap-2 text-sm ${
+                    theme === "dark" ? "text-gray-400" : "text-gray-600"
+                  }`}
+                >
+                  <Phone className="w-4 h-4" /> {r.phone_number}
+                </div>
+                <div
+                  className={`flex items-center justify-center gap-2 text-sm ${
+                    theme === "dark" ? "text-gray-400" : "text-gray-600"
+                  }`}
+                >
+                  <Mail className="w-4 h-4" /> {r.email}
+                </div>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+      {error ? (
+        <p className="mt-4 text-center text-red-600 dark:text-red-400 font-semibold">
+          {error}
+        </p>
+      ) : (
+        selectedProfile && (
+          <div
+            className={`w-[50%] flex items-center justify-between mt-2 text-sm font-medium ${
+              theme === "dark" ? "text-gray-300" : "text-gray-700"
+            }`}
+          >
+            <div>
+              <span className="font-semibold">{selectedProfile.name}</span>{" "}
+              {"-"}{" "}
+              <span className="font-semibold">{selectedProfile.email}</span>
+            </div>
+          </div>
+        )
+      )}
       <div className="mt-8 flex justify-center gap-6 max-w-sm mx-auto">
         {isUserListsRoute && (
           <button
             onClick={onClose}
-            className="flex-1 bg-gray-300 dark:bg-gray-700 hover:bg-gray-400 dark:hover:bg-gray-600 text-gray-900 dark:text-gray-100 font-semibold px-6 py-3 rounded-md shadow-sm transition"
+            className={`flex-1 ${
+              theme === "dark"
+                ? "bg-gray-700 hover:bg-gray-600 text-gray-100"
+                : "bg-gray-300 hover:bg-gray-400 text-gray-900"
+            } font-semibold px-6 py-3 rounded-md shadow-sm transition`}
             aria-label="Volver"
           >
             Volver
@@ -42,16 +176,13 @@ const PhoneStep: React.FC<PhoneStepProps> = ({
         <button
           disabled={!phone}
           onClick={onSubmit}
-          className="flex-1 bg-blue-600 hover:bg-blue-700 disabled:bg-blue-300 disabled:cursor-not-allowed text-white font-semibold px-6 py-3 rounded-md shadow-sm transition"
+          className={
+            "flex-1 bg-blue-600 hover:bg-blue-700 disabled:bg-blue-300 disabled:cursor-not-allowed text-white font-semibold px-6 py-3 rounded-md shadow-sm transition"
+          }
         >
           Siguiente
         </button>
       </div>
-      {error && (
-        <p className="mt-4 text-center text-red-600 dark:text-red-400 font-semibold">
-          {error}
-        </p>
-      )}
     </div>
   );
 };
